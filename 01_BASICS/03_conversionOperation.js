99006b7aeb01de418b78f6063296fec2d62e7396
Rakesh Joshi <rakeshjoshiamravati@gmail.com>
2025-12-10 10:42:47 +0000

    conversion of Data Types

---
+++ 01_BASICS/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score  = "33abc"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let score2 = null
+console.log(typeof score2)
+console.log(typeof(score2))
+
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+let score3 = undefined
+console.log(typeof score3)
+console.log(typeof(score3))
+
+let valueInNumber3 = Number(score3)
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3)
+
+
+
+let score4 = true
+console.log(typeof score4)
+console.log(typeof(score4))
+
+let valueInNumber4 = Number(score4)
+console.log(typeof valueInNumber4)
+console.log(valueInNumber4)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => treue; 0 => false
+// "" => false
+// "Rakesh" => true
+
+let sum  = 33
+let stringnum = String(sum)
+console.log(stringnum)
+console.log(typeof stringnum)
+
