289c9013e2b788a1aebe1b195c3a115c9bf45a28
Rakesh Joshi <rakeshjoshiamravati@gmail.com>
2025-12-10 10:25:55 +0000

    datatypes

---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all JS code as a newer version
+// alert(3+3) we r using NodeJS not browser
+ console.log(3+3)
+ console.log("Rakesh")
+ 
+
+ let name = "Rakesh"
+ let age = 22
+ let isLoggedIn = false
+
+
+ //number 
+ //bigint
+ //string => '' or " "
+ //boolean =? true/false
+ //null =>standalone value
+ //undefined =>
+ //symbol => unique
+
+ //object
+ console.log(typeof "Rakesh")
+ console.log(typeof age)
+ console.log(typeof null)
+ console.log(typeof undefined)
+
+ 
